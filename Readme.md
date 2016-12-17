fd13d8dc611998fae7f615815c03e75386e064f4
rexxh <rexxh@mail.ru>
2016-12-17 13:23:34 +0300

    1

---
+++ Readme.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/TAHK6e3TOPMO3OB/sort_by_TAHK.svg?branch=master)](https://travis-ci.org/TAHK6e3TOPMO3OB/sort_by_TAHK)
---
+++ tests/init.cpp
@@ -0,0 +1,75 @@
+#include <sort_by_TAHK.cpp>
+#include <catch.hpp>
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+SCENARIO("8mb", "[8mb]")
+{
+ sort_by_TAHK("8mb", "out_8", 1);
+  bool x = true;
+  std::ifstream f1("out_8"), f2("out8");
+  man m1, m2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>m1)&&(f2>>m2))
+    {
+      if(m1.name!=m2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
+}
+
+SCENARIO("15mb", "[15mb]")
+{
+  sort_by_TAHK("15mb", "out_15", 4);
+  bool x = true;
+  std::ifstream f1("out_15"), f2("out15");
+  man m1, m2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>m1)&&(f2>>m2))
+    {
+      if(m1.name!=m2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
+}
+
+SCENARIO("32mb", "[32mb]")
+{
+  sort_by_TAHK("32mb", "out_32", 17);
+  bool x = true;
+  std::ifstream f1("out_32"), f2("out32");
+  man m1, m2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>m1)&&(f2>>m2))
+    {
+      if(m1.name!=m2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
+}
