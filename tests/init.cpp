8c3f13c5ae0099082fdaa783265b638d54352957
rexxh <rexxh@mail.ru>
2016-12-17 15:34:00 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 using namespace std;
  
-SCENARIO("8mb", "[8mb]")
+/*SCENARIO("8mb", "[8mb]")
 {
  sorting ("8mb", "out_8", 1);
   bool x = true;
@@ -56,6 +56,28 @@ SCENARIO("32mb", "[32mb]")
   sorting ("32mb", "out_32", 17);
   bool x = true;
   std::ifstream f1("out_32"), f2("out32");
+  man m1, m2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>m1)&&(f2>>m2))
+    {
+      if(m1.name!=m2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
+}*/
+SCENARIO("8mb", "[8mb]")
+{
+  sorting ("Unsorted8.txt", "sorted8", 1);
+  bool x = true;
+  std::ifstream f1("out_8"), f2("out8");
   man m1, m2;
   while(!f2.eof() && !f1.eof())
   {
